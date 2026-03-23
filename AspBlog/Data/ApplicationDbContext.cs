cf97e00eaffa2dfbe787ebe964387b9a6d7965d3
Pechy6 <Pechyy6@gmail.com>
2026-03-23 20:23:51 +0100

    Add Article Entity and update DbContext

--- AspBlog/Data/ApplicationDbContext.cs
+++ AspBlog/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using AspBlog.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AspBlog.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
+    public DbSet<Article> Articles { get; set; }
 }
---
+++ AspBlog/Models/Entities/Article.cs
@@ -0,0 +1,9 @@
+namespace AspBlog.Models.Entities;
+
+public class Article
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+}
